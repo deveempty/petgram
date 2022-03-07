81a68844a6b16ada50ff7c776d27f26760f5c2c3
Juan Viloria <juanviloriv@gmail.com>
2022-03-07 00:39:53 -0500

    Creating components and styled components
    ;

---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Category } from '../Category'
+
+import { List, Item } from './styles'
+import data from '../../../api/db.json'
+
+export const ListOfCategories = () => {
+  return (
+    <List>
+      {
+        data.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+}
