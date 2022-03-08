513bfa21e1f08d81b8e7dc7ae38c78f3758c45ae
Juan Viloria <juanviloriav@gmail.com>
2022-03-08 01:15:43 -0500

    New bar pop-ups when scroll down - animation created

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, {Fragment, useEffect, useState} from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import data from '../../../api/db.json'
+// import data from '../../../api/db.json' <-- don't need this, but i'll save it here :) 
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+ // Hook.
+  useEffect(function () {
+    fetch('https://petgram-server-rsor.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+////
+//Top ListCategories show up when scroll down 200 px
+  useEffect(function () {
+    const onScroll = e =>{
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll) // Avoid memory leak. 
+  }, [showFixed])
+//////// Render list to show categories - This will be use again at the return to get a mini one!  ////
+  const renderList = (fixed) => (
+      <List className={fixed ? 'fixed' : ''}>
+        {
+          categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        }
+      </List>
+  )
+
   return (
-    <List>
-      {
-        data.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-      }
-    </List>
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,9 +1,33 @@
 import styled from 'styled-components'
+import { appearFromTop } from '../../styles/animation'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  //Hidden scroll without affect functionability 
+  &::-webkit-scrollbar {
+        display: none;
+    };
+  &.fixed {   
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0,0,0,0.3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(.5);
+    z-index: 1;
+    ${appearFromTop({
+      time: '300ms',
+      from: '-25%',
+      to: '-40px',
+    })}
+  }
 `
 
 export const Item = styled.li`
