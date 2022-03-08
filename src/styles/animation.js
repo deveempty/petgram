405e5f16790582dc26d8adcc75d040ca04f85fd3
Juan Viloria <juanviloriav@gmail.com>
2022-03-08 01:17:13 -0500

    animation for bar on categories created

--- src/styles/animation.js
+++ src/styles/animation.js
@@ -1,5 +1,6 @@
 import { keyframes, css } from 'styled-components'
 
+/// Animation for photocard
 const fadeInKeyframes = keyframes`
   from {
     filter: blur(5px);
@@ -14,3 +15,22 @@ const fadeInKeyframes = keyframes`
 
 export const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
   css`animation: ${time} ${fadeInKeyframes} ${type};`
+///; Animation for Mini bar on ListOfCategories
+const appearFromTopKeyframes = (from, to) => {
+  return keyframes`
+  from{
+    top: ${from};
+  }
+
+  to{
+    top: ${to};
+  }
+  `
+}
+
+export const appearFromTop = ({
+  time = '1s',
+  easing = 'cubic-bezier(.18,.89,.32,1.28)',
+  from,
+  to,
+} = {}) => css`animation: ${time} ${appearFromTopKeyframes(from, to)} ${easing}, ${time} ${fadeInKeyframes} ease`
